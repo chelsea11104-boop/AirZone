b817138cb4560b7e330bdab41aaa03aaabc65d56
chelsea11104-boop <chelsea11104@gmail.com>
2025-10-17 20:06:06 +0700

    kecilkan judul tampilan

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,9 @@ import MapView from "../components/MapView";
 const HomePage = () => {
   return (
     <div className="bg-gradient-to-b from-blue-100 to-green-100 min-h-screen">
+      <h1 className="text-center text-2x1 font-bold text-blue-700 py-4">
+        AirZone - Peta Kualitas Udara Regional
+      </h1>
       <MapView />
     </div>
   );
